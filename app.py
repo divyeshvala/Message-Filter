7d28d649b7714995fa0881f6ab41a329462bf0d9
divyeshvala <divyeshvala228@gmail.com>
2020-11-25 22:08:34 +0530

    New design added for Homepage and old homepage has been removed.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
